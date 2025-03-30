3880b5abda888aa6b704978c71a5567be47c2f26
Zaidan Rizq <rizq.zaidan21@gmail.com>
2025-03-30 17:56:02 +0700

    Added kriteria dan saran codes for submission

---
+++ build_push_image_karsajobs_ui.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# Set konfigurasi keluar dari script bila ada error
+set -e
+
+# Build image dari Dockerfile
+echo "Building Docker Image..."
+docker build -t ghcr.io/zaidanrizq/karsajobs-ui:latest .
+
+
+# Login ke GitHub Container Registry
+echo -e "\nLogin to GitHub Container Registry..."
+echo $CR_PAT | docker login ghcr.io -u $USERNAME --password-stdin
+
+# Push Docker Image ke GitHub Container Registry
+echo -e "\nPushing Image to GitHub Container Registry..."
+docker push ghcr.io/zaidanrizq/karsajobs-ui:latest
