d425185b11386848ddfbb6fc471d5a099251edca
Zaidan Rizq <rizq.zaidan21@gmail.com>
2025-03-30 16:50:58 +0700

    Added kriteria dan saran code for submission

---
+++ build_push_image_karsajobs.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Set konfigurasi keluar dari script bila ada error
+set -e
+
+# Build image dari Dockerfile
+echo "Building Docker Image..."
+docker build -t ghcri.io/zaidanrizq/karsajobs:latest .
+
+
+# Login ke GitHub Container Registry
+echo -e "\nLogin to GitHub Container Registry..."
+echo $CR_PAT | docker login ghcr.io -u $USERNAME --password-stdin
+
+# Push Docker Image ke GitHub Container Registry
+echo -e "\nPushing Image to GitHub Container Registry..."
+docker push ghcr.io/zaidanrizq/karsajobs:latest
