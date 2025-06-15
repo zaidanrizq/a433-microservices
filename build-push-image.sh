68ab5048475c5d5031908844bea447c6cf923b63
Zaidan Rizq <rizq.zaidan21@gmail.com>
2025-06-15 13:39:39 +0000

    Add DOckerfile and build push image script

---
+++ build-push-image.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+set -eo pipefail
+
+IMAGE="ghcr.io/$USERNAME_GITHUB/shippingservice:latest"
+
+echo "🔨 Building Docker image…"
+docker build --target production -t "$IMAGE" .
+
+echo "🔐 Logging in to GitHub Container Registry…"
+echo "$CR_PAT" | docker login ghcr.io -u "$USERNAME_GITHUB" --password-stdin
+
+echo "📤 Pushing image to $IMAGE…"
+docker push "$IMAGE"
